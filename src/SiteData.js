98c5337af5b501e64848e0bf0ed4ae36b1affb51
Tracy Parks <me@tracy.is>
2022-11-26 09:00:39 -0800

    meta, css, and site data setup

---
+++ src/SiteData.js
@@ -0,0 +1,64 @@
+const SiteData = {
+    "version": "0.1.0",
+    "head":{
+        "title": "Star Fleet Battles Companion App",
+        "meta":[
+            {
+                "charSet":"utf-8"
+            },
+            {
+                "httpEquiv":"X-UA-Compatible",
+                "content":"IE=edge, chrome=1"
+            },
+            {
+                "name": "description",
+                "content": "The description"
+            },
+            {
+                "name": "author",
+                "content": "Tracy Parks"
+            },
+            {
+                "name": "viewport",
+                "content": "width=device-width, initial-scale=1"
+            },
+            {
+                "name": "generator",
+                "content": "custom REACT"
+            }
+        ],
+        "link":[
+            {
+                "rel": "stylesheet",
+                "href": "./css/normalize.css"
+            },
+            {
+                "rel": "stylesheet",
+                "href": "./css/main.css"
+            },
+            {
+                "rel": "apple-touch-icon",
+                "sizes": "180x180",
+                "href": "images/apple-touch-icon.png"
+            },
+            {
+                "rel": "icon",
+                "sizes": "32x32",
+                "type": "image/png",
+                "href": "images/favicon-32x32.png"
+            },
+            {
+                "rel": "icon",
+                "sizes": "16x16",
+                "type": "image/png",
+                "href": "images/favicon-16x16.png"
+            },
+            {
+                "rel": "manifest",
+                "href": "manifest.json"
+            }
+        ]
+    }
+}
+
+export default SiteData;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import SiteData from './SiteData';
+
+let meta = [];
+
+Object.keys(SiteData.head).forEach(key => {
+    if(key === 'meta' || key === 'link'){
+        const CustomTag = `${key}`;
+        SiteData.head[key].forEach((item,index) => {
+            meta.push(<CustomTag key={key+index} {...item} />);
+        });
+    } else {
+        const CustomTag = `${key}`;
+        meta.push(<CustomTag key={key}> {SiteData.head[key]} </CustomTag>);
+    }
+});
+
+const head = ReactDOM.createRoot(document.head);
+head.render(
+    <>
+    {meta}
+    </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
